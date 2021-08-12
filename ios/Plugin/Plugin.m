8bee7e6c25eb85e0f35cb98ed6f52e6e98f9c27a
liubei <beiliubei@gmail.com>
2021-08-12 22:59:52 +0800

    fix bug

--- ios/Plugin/Plugin.m
+++ ios/Plugin/Plugin.m
@@ -3,7 +3,7 @@
 
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
-CAP_PLUGIN(CapacitorGaodeMaps, "CapacitorGaodeMaps",
+CAP_PLUGIN(NativeGaoDeMap, "NativeGaoDeMap",
            CAP_PLUGIN_METHOD(initialize, CAPPluginReturnPromise);
 //            CAP_PLUGIN_METHOD(create, CAPPluginReturnPromise);
 //            CAP_PLUGIN_METHOD(addMarker, CAPPluginReturnPromise);
