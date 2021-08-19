020344f45e6c87696bd9674a503ce3cb214dbdf7
liubei <beiliubei@gmail.com>
2021-08-19 14:33:25 +0800

    android 定位

--- android/src/main/java/com/getcapacitor/community/NativeGaoDeMap.java
+++ android/src/main/java/com/getcapacitor/community/NativeGaoDeMap.java
@@ -1,10 +1,11 @@
 package com.getcapacitor.community;
 
 import android.Manifest;
-import android.content.Context;
-import android.content.res.AssetFileDescriptor;
-import android.util.Log;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
@@ -12,21 +13,54 @@ import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 
 @NativePlugin(
-    permissions = {
-        Manifest.permission.WRITE_EXTERNAL_STORAGE
-    }
+        permissions = {
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+        }
 )
 public class NativeGaoDeMap extends Plugin {
+    public AMapLocationClient mLocationClient = null;
 
     public static final String TAG = "NativeGaoDeMap";
 
     @PluginMethod()
     public void initialize(final PluginCall call) {
-
+        mLocationClient = new AMapLocationClient(getContext());
+        AMapLocationClientOption mLocationOption = new AMapLocationClientOption();
+        mLocationOption.setOnceLocation(true);
+        mLocationOption.setOnceLocationLatest(true);
+        mLocationOption.setNeedAddress(true);
+        //给定位客户端对象设置定位参数
+        mLocationClient.setLocationOption(mLocationOption);
     }
 
     @PluginMethod()
     public void myLocation(final PluginCall call) {
-
+        AMapLocationListener mLocationListener = new AMapLocationListener() {
+            @Override
+            public void onLocationChanged(AMapLocation aMapLocation) {
+                mLocationClient.stopLocation();
+                if (aMapLocation != null) {
+                    if (aMapLocation.getErrorCode() == 0) {
+                    //可在其中解析amapLocation获取相应内容。
+                        JSObject jsonObject = new JSObject();
+                        jsonObject.put("latitude", aMapLocation.getLatitude());
+                        jsonObject.put("longitude", aMapLocation.getLongitude());
+                        jsonObject.put("address", aMapLocation.getAddress());
+                        call.resolve(jsonObject);
+                    } else {
+                        //定位失败时，可通过ErrCode（错误码）信息来确定失败的原因，errInfo是错误信息，详见错误码表。
+                        call.error("location Error, ErrCode:"
+                                + aMapLocation.getErrorCode() + ", errInfo:"
+                                + aMapLocation.getErrorInfo());
+                    }
+                }else {
+                    call.error("定位失败");
+                }
+            }
+        };
+        mLocationClient.setLocationListener(mLocationListener);
+        //启动定位
+        mLocationClient.startLocation();
     }
 }
